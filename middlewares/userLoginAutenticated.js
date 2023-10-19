a94ab3e00dd19f51de93eafacf1416d23854a455
FrancoSassi03 <francosassi03@gmail.com>
2023-10-19 18:45:30 -0300

    ejercicio 2
    ejercicio 2

--- middlewares/userLoginAutenticated.js
+++ middlewares/userLoginAutenticated.js
@@ -3,12 +3,9 @@ const { Article } = require("../models");
 async function userLoginAutenticated(req, res, next) {
   if (req.isAuthenticated()) {
     const article = await Article.findByPk(req.params.id);
-    if (article.userId === req.user.id) {
       if (req.user.role.code >= 200) {
        return next(); 
       }
-      res.redirect("/");
-    }
     res.redirect("/admin");
   } else {
     res.redirect("/login");
